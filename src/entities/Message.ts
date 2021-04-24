fddb98e25c12dffaee32cf08b3c9a853b21a61bd
mariana <marianarduini@gmail.com>
2021-04-24 14:40:38 -0300

    initial commit

---
+++ src/entities/Message.ts
@@ -0,0 +1,35 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, ManyToOne, JoinColumn } from "typeorm";
+
+import { v4 as uuid } from "uuid";
+import { User } from "./User";
+
+@Entity("messages")
+class Message{
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  admin_id: string;
+
+  @Column()
+  text: string;
+
+  @JoinColumn({ name: "user_id"})
+  @ManyToOne(() => User) //many messages to one user
+  user: User; // carry the User object, not only the user_id
+
+  @Column() //db server will handle the FK
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor(){
+    if (!this.id){
+      this.id = uuid();
+    }
+  }
+
+}
+
+export { Message };
